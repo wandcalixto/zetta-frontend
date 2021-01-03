98d42224bccbbb7c7264eb91cb863c18acd734ad
wanderson <wanderson.silva@doctum.edu.br>
2021-01-03 16:29:54 -0300

    adicionando link para instruções na apresentação

--- src/components/apresentacao.vue
+++ src/components/apresentacao.vue
@@ -9,10 +9,10 @@
         <b>Olá,</b>
         <br>
         Eu me chamo Wanderson Calito da Silva, sou formado em Ciência da Computação e trabalho há três anos como programador.<br>
-        Esta aplicação foi criada a partir do Desafio Tecnico do processo seletivo da Agência Zetta.<br>
+        Esta aplicação foi criada a partir do Desafio Prático do processo seletivo da Agência Zetta.<br>
         Entre as diversas tecnologias utilizadas no desenvolvimento, pode-se listar:
        <ul>
-            <li><b>Back-end</b><ul>
+            <li><b>Backend</b><ul>
                     <li>Linguagem: Java</li>
                     <li>Framework Java: Spring Boot</li>
                     <li>Banco de Dados: PostgreSQL</li>
@@ -21,7 +21,7 @@
             </li>
         </ul>
         <ul>
-            <li><b>Front-end</b><ul>
+            <li><b>Frontend</b><ul>
                     <li>Framework Javascript: Vue.js</li>
                     <li>Framework Web: BootStrap</li>                  
                 </ul>
@@ -29,15 +29,17 @@
         </ul>
          <ul>
             <li><b>Hospedagem</b><ul>
-                    <li>Back-end: <a href="https://apirest-zetta.herokuapp.com/swagger-ui.html#/" target="_blank">Heroku</a></li>
-                    <li>Front-end: <a href="http://wandersonzetta.online/#/" target="_blank">Umbler</a></li>                  
+                    <li>Backend: <a href="https://apirest-zetta.herokuapp.com/swagger-ui.html#/" target="_blank">Heroku</a></li>
+                    <li>Frontend: <a href="http://wandersonzetta.online/#/" target="_blank">Umbler</a></li>                  
                 </ul>
             </li>
         </ul>
         <ul>
             <li><b>Repostório</b><ul>
-                    <li><a href="https://github.com/wandcalixto/zetta-backend" target="_blank">Back-end</a></li>
-                    <li><a href="https://github.com/wandcalixto/zetta-frontend" target="_blank">Front-end</a></li>                  
+                    <li><a href="https://github.com/wandcalixto/zetta-backend" target="_blank">Backend</a></li>
+                    <li><a href="https://github.com/wandcalixto/zetta-frontend" target="_blank">Frontend</a></li> 
+                    <li><a href="https://drive.google.com/file/d/1J9qae63PRTfX7swFnwie_FwOhyQcpzpD/view?usp=sharing" target="_blank">Instruções</a></li>      
+                               
                 </ul>
             </li>
         </ul>
